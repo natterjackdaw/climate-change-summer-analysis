bb5d39afd1289a88eb46025379d112820c96153c
natterjackdaw <58046363+natterjackdaw@users.noreply.github.com>
2025-12-12 23:38:57 +0000

    slight change

--- calc_clim_mean.py
+++ calc_clim_mean.py
@@ -4,13 +4,15 @@ import xarray as xr
 
 
 # Ask Xarray to not show data values by default
-xr.set_options(display_expand_data=False)
+# xr.set_options(display_expand_data=False)
 
 data_path = f"{os.getcwd()}/data"
 
-ds_grib = xr.open_dataset(f"{data_path}/2m_temperature_europe_195007_alltime.grib", 
-                        engine="cfgrib")
+ds_grib = xr.open_dataset(
+    f"{data_path}/monthly_mean_2m_temperature_europe_195006-202508.grib", 
+    engine="cfgrib"
+)
 
-ds_mean = ds_grib.mean(["time", "step"])
+print(ds_grib["time.year"])
 
-print(ds_mean)
+# use group by
