38763ff9163dc95cc7bb8283d471a98499396d16
natterjackdaw <58046363+natterjackdaw@users.noreply.github.com>
2025-12-14 20:38:36 +0000

    save summer mean as netcdf code

--- 01_save_summer_averages.py
+++ 01_save_summer_averages.py
@@ -3,11 +3,11 @@ import os
 import xarray as xr
 
 
-# Ask Xarray to not show data values by default
-# xr.set_options(display_expand_data=False)
-
 data_dir = f"{os.getcwd()}/data"
+
+# future work: make these less manual
 data_path = f"{data_dir}/monthly_mean_2m_temperature_europe_195006-202508.grib"
+out_path = f"{data_dir}/summer_mean_2m_temperature_europe_1950-2025.nc"
 
 print(f"Reading {data_path}")
 ds_grib = xr.open_dataset(
@@ -17,3 +17,9 @@ ds_grib = xr.open_dataset(
 # mean summer temperature, not monthly mean
 ds_summer = ds_grib.groupby('time.year').mean('time')
 
+# save as netCDF
+print(f"Saving summer means to {out_path}...")
+ds_summer.to_netcdf(
+    path=out_path,
+    mode="a" # in case we add future years
+)
