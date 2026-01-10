e1b5756e0655f9bc15267625b1b724c6ea5dff87
natterjackdaw <58046363+natterjackdaw@users.noreply.github.com>
2026-01-10 22:13:43 +0000

    rough code in exploration to make ugly plot

--- exploration/plot_if_year_is_highest_so_far.py
+++ exploration/plot_if_year_is_highest_so_far.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import xarray as xr
 
-from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import Basemap, addcyclic
 import matplotlib.pyplot as plt
 
 
@@ -21,6 +21,7 @@ lons = ds["longitude"].to_numpy()
 lats = ds["latitude"].to_numpy()
 print(da.shape, lons.shape, lats.shape)
 
+
 # calculate the maximum mean summer temperature
 max_summer = da.max(axis=0)
 print(max_summer.shape)
@@ -29,17 +30,39 @@ latest_year = da[-1,:,:]
 
 diff = latest_year - max_summer
 
-is_hottest = np.where(diff == 0, 1, None)
+is_hottest = np.where(diff == 0, 1, 0)
 print(is_hottest.shape)
 
 # convert lats and lons to list
-lats = lats.tolist()
-lons = lons.tolist()
+# lats = lats.tolist()
+# lons = lons.tolist()
+
+# print(lons)
+# print(lats)
 
 # plot
 # create the figure and axes instances.
 
 fig = plt.figure()
-ax = fig.add_axes([0.1,0.1,0.8,0.8])
+# ax = fig.add_axes([0.05,0.1,0.9,0.9])
+
+map = Basemap(
+    lon_0 = 0,
+    llcrnrlat=lats[-1],
+    urcrnrlat=lats[0],
+    llcrnrlon=lons[0],
+    urcrnrlon=lons[-1]
+)
+
+map.drawcoastlines(linewidth=0.2)
+is_hottest = np.array(is_hottest, dtype=float)
+# not sure what this does yet
+is_hottest, lons = addcyclic(is_hottest, lons)
+xx, yy   = np.meshgrid(lons,lats)
+xx, yy    = map(xx, yy)
+
+conf = map.contourf(xx, yy, is_hottest)
+fig.colorbar(conf,orientation='horizontal')
 
 
+plt.savefig('images/test.png')
--- src/map_helpers.py
+++ src/map_helpers.py
@@ -1,20 +1,36 @@
 import numpy as np
 from typing import List, Dict
 
-def rearrange_for_prime_meridian(
-    map_data: np.numpy,
-    lons: List
-) -> Dict:
-    '''
-    Re-arranges data so that it may be mapped with a focus on Europe or Africa 
-    (i.e., the starting point is not 0, and there are negative latitudes, 
-    rather than 0-360).
+# def rearrange_for_prime_meridian(
+#     map_data: np.numpy,
+#     lons: List,
+#     axis: int = 1
+# ) -> Dict:
+#     '''
+#     Re-arranges data so that it may be mapped with a focus on Europe or Africa 
+#     (i.e., the starting point is not 0). Therefore, longitudes become between 
+#     -180 and 180, rather than values within 0-360).
     
-    :param map_data: The data to be mapped on a plot.
-    :type map_data: np.numpy
-    :param lons: The latitudes represented by the data.
-    :type lons: List
-    :return: The converted latitude and data.
-    :rtype: Dict
-    '''
-    # check that the shape of array matches the length of the longitude points
+#     :param map_data: The data to be mapped on a plot.
+#     :type map_data: np.numpy
+#     :param lons: The latitudes represented by the data.
+#     :type lons: List
+#     :param axis: which axis of the data is the longitude, where first is 0.
+#     :type axis: int
+#     :default axis: 1 (which would be the longitudes in a 2D (lon,lat) array)
+#     :return: The converted latitude and data.
+#     :rtype: Dict
+#     '''
+#     lon_len_in_data = map_data.shape()[axis]
+#     # check that the shape of array matches the length of the longitude points
+#     if lon_len_in_data != len(lons):
+#         error_explanation = f"""Looks like the longitude input does not match
+#         the shape of the data.
+#         Are you using the correct axis? {axis}
+#         The data shape is {map_data.shape()} and so the longitude expected is 
+#         {lon_len_in_data}.
+#         """
+#         raise ValueError(error_explanation)
+    
+    # need to know at what point longitude has a value above 180
+    
