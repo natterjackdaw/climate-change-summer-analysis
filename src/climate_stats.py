956aa87e1011a1b8c11869513a3aa5e24e1fd14d
natterjackdaw <58046363+natterjackdaw@users.noreply.github.com>
2026-01-30 22:03:48 +0000

    finish percentile function without testing

--- src/climate_stats.py
+++ src/climate_stats.py
@@ -1,26 +1,62 @@
 import numpy as np
+from itertools import product
 from scipy.stats import percentileofscore
 from typing import List
 
 
-# def convert_measurements_to_percentile_one_direction(
-#         measurements: np.array,
-#         axis: int
-# ) -> np.array:
+def calc_percentile_3d_array(
+        data: np.array,
+        index_of_score: int,
+        axis: int = 0
+) -> np.array:
+    '''
+    Calculates the percentile for a value at a user-defined index based on 
+    other values over one axis in a 3d array.
     
+    :param data: The 3d data (e.g., time x lat x lon)
+    :type data: np.array
+    :param index_of_score: Index of value to calc score for
+    :type index_of_score: int
+    :param axis: Over which axis (default = 0; e.g., time)
+    :type axis: int
+    :return: a 2d array containing percentiles
+    :rtype: np.array
+    '''
 
+    if axis > 2:
+        raise ValueError("axis must be 0, 1, or 2")
+
+    axis_dict = {}
+    try:
+        axis_dict[0], axis_dict[1], axis_dict[2] = data.shape
+    except:
+        raise ValueError(f"data array is not the correct shape: {data.shape}")
     
-    
+    # do not want to loop through axis that we are calculating over
+    del axis_dict[axis]
+    # dimentions to loop through
+    len4loop = [v for v in axis_dict.values()]
 
+    a_result = np.empty([len4loop[0], len4loop[1]])
+    for j, i in product(range(len4loop[0]), range(len4loop[1])):
 
-# if __name__ == "__main__":
+        try:
+            if axis == 0:
+                a = data[:,j,i]
+                s = data[index_of_score,j,i]
+            elif axis == 1:
+                a = data[j,:,i]
+                s = data[j, index_of_score, i]
+            else:
+                a = data[j,i,:]
+                s = data[j, i, index_of_score]
+        
+            a_result[j,i] = percentileofscore(a, s)
 
-#     rough_test = [1, 25, 100, 50, 95, 83, 32, 104]
-
-#     out_test = convert_measurements_to_percentile_one_direction(rough_test)
-
-#     print(rough_test)
-#     print(out_test)
-
-#     print('compare to scipy.stats')
-#     print(percentileofscore(rough_test, rough_test))
+        except:
+            raise ValueError(
+                f"""{j} or {i} is too big for calc over axis {axis} 
+                for shape {data.shape}
+                """)
+        
+    return a_result
